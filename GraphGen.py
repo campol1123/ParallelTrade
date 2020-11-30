28edc195042532085ff3fd826754ecbf6e3b325a
campol1123 <75270817+campol1123@users.noreply.github.com>
2020-11-30 20:41:39 +0000

    Update GraphGen.py

--- GraphGen.py
+++ GraphGen.py
@@ -6,6 +6,7 @@ list_time = []
 
 rand_mp = input("enter random multiplier: ")
 
+# generates numbers for graph, and inserts the 0-100 time values
 for x in range (100):
     rand_add = random.randint(1,int(rand_mp))
     list_time.append(x)
@@ -13,6 +14,7 @@ for x in range (100):
 print(list_price)
 print(list_time)
 
+#converts graph values to applicable coordinates
 for x in range (len(list_price)):
     list_price[x] *= 6.7
     list_price[x] -= 335
@@ -24,13 +26,19 @@ print(len(list_time))
 bn1 = 0
 bn2 = 0
 
-    
+#finds highest price (currently useless)
+for x in range(len(list_price)):
+    if list_price[x] > bn1:
+        bn1 = list_price[x]
+
+#a test formula for working out the average graph
+rm = list_price[len(list_price)-1]
 
 
 
 
 
-
+#turtle instructions
 turtle.penup()
 turtle.goto(-335,-275)
 turtle.pendown()
@@ -38,5 +46,13 @@ for x in range (len(list_price)):
     turtle.goto(list_time[x], list_price[x])
 turtle.penup()
 turtle.goto(-335,-275)
+turtle.pendown()
+turtle.color("red")
+turtle.goto(list_time[len(list_time)-1],list_price[len(list_price)-1])
+turtle.goto(-335,-275)
+for x in range (len(list_price)):
+    turtle.goto(list_time[x], (list_time[x]*rm))
+print(len(list_time))
 
+#just to keep the turtle tab open
 input()
