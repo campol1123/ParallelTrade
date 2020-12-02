8e3c95b9fa3c89417866d2d603fef9ad04902708
campol1123 <75270817+campol1123@users.noreply.github.com>
2020-12-02 21:01:01 +0000

    Update GraphGen.py
    nearly there:
    - new way of graph gen
    - has a line representing a linear increase
    - nearly accurate parallel lines

--- GraphGen.py
+++ GraphGen.py
@@ -1,58 +1,85 @@
-import random
 import turtle
+import random
 
 list_price = []
 list_time = []
+n_list_p = []
+n_list_t = []
+d_list = []
+ulc = []
+llc = []
+turtle.speed(0)
 
-rand_mp = input("enter random multiplier: ")
-
-# generates numbers for graph, and inserts the 0-100 time values
-for x in range (100):
-    rand_add = random.randint(1,int(rand_mp))
+for x in range (101):
+    list_price.append(x)
     list_time.append(x)
-    list_price.append(x + rand_add)
-print(list_price)
-print(list_time)
 
-#converts graph values to applicable coordinates
-for x in range (len(list_price)):
-    list_price[x] *= 6.7
-    list_price[x] -= 335
-    list_time[x] *= 5.5
-    list_time[x] -= 275
-print(list_price)
-print(list_time)
-print(len(list_time))
-bn1 = 0
-bn2 = 0
-
-#finds highest price (currently useless)
-for x in range(len(list_price)):
-    if list_price[x] > bn1:
-        bn1 = list_price[x]
-
-#a test formula for working out the average graph
-rm = list_price[len(list_price)-1]
-
-
-
-
-
-#turtle instructions
 turtle.penup()
-turtle.goto(-335,-275)
+fc1 = (list_time[0] * 6) - 300
+fc2 = (list_price[0]-100)
+turtle.goto(fc1, fc2)
 turtle.pendown()
-for x in range (len(list_price)):
-    turtle.goto(list_time[x], list_price[x])
+
+for x in range(99):
+    x = x+1
+    c1 = (list_time[x]*6)-300
+    c2 = (list_price[x]+random.randint(-20,150)-50)
+    turtle.goto(c1,c2)
+    n_list_t.append(c1)
+    n_list_p.append(c2)
+
+turtle.goto((list_time[len(list_time)-1]*6)-300,(list_price[len(list_price)-1])-50)
 turtle.penup()
-turtle.goto(-335,-275)
-turtle.pendown()
+
+turtle.goto(fc1,fc2)
 turtle.color("red")
-turtle.goto(list_time[len(list_time)-1],list_price[len(list_price)-1])
-turtle.goto(-335,-275)
-for x in range (len(list_price)):
-    turtle.goto(list_time[x], (list_time[x]*rm))
-print(len(list_time))
+turtle.pendown()
+turtle.goto((list_time[len(list_time)-1]*6)-300,(list_price[len(list_price)-1])-50)
 
-#just to keep the turtle tab open
+end_price = list_price[len(list_price)-1]
+print(end_price)
+
+end_price2 = list_price[len(list_price)-2]
+print(end_price2)
+
+pm = n_list_p[-1]/100
+for x in range (99):
+    n = n_list_p[x]-(pm*n_list_t[x])
+    d_list.append(n)
+bn = 0
+sn = 0
+for x in range(len(d_list)):
+    if bn < d_list[x]:
+        bn = d_list[x]
+    if d_list[x] < sn:
+        sn = d_list[x]
+for x in range(len(d_list)):
+    ulc.append((pm*n_list_t[x])+bn)
+    llc.append((pm * n_list_t[x]) + sn)
+
+turtle.penup()
+turtle.goto(fc1, fc2)
+turtle.pendown()
+
+for x in range(len(d_list)):
+    turtle.goto(n_list_t[x],ulc[x])
+turtle.penup()
+turtle.goto(fc1, fc2)
+turtle.pendown()
+
+for x in range(len(d_list)):
+    turtle.goto(n_list_t[x],llc[x])
+
+
+print(llc)
+print(ulc)
+print (sn)
+print(bn)
+print (d_list)
+
+
+
+
+
+
 input()
