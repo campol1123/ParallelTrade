e8066a463c02dbc4416e227a0a1c612aa68c0167
campol1123 <75270817+campol1123@users.noreply.github.com>
2020-12-04 20:55:47 +0000

    Update GraphGen.py
    -basic algorithm completed

--- GraphGen.py
+++ GraphGen.py
@@ -4,12 +4,13 @@ import random
 ulc = []
 llc = []
 n_list_p = []
+n_list_dp = []
 n_list_t = []
 d_list = []
 turtle.speed(0)
 ft = 1
 
-for x in range (120):
+for x in range (50):
     if ft == 1:
         turtle.penup()
     else:
@@ -17,21 +18,52 @@ for x in range (120):
     x += 1
     ft = 0
     n_list_t.append((x*3)-300)
-    n_list_p.append(((x*4)+(random.randint(9,12)*6))-300)
+    n_list_p.append(((x * 5) - 300) + (random.randint(-20, 120)))
+    n_list_dp.append(((x * 5) - 300))
     turtle.goto(n_list_t[x-1],n_list_p[x-1])
-turtle.penup()
-turtle.goto(n_list_t[0],n_list_p[0])
+
+ft = 1
 turtle.color("red")
-turtle.pendown()
-turtle.goto(n_list_t[-1],n_list_p[-1])
+for x in range (50):
+    if ft == 1:
+        turtle.penup()
+    else:
+        turtle.pendown()
+    x += 1
+    ft = 0
+    turtle.goto(n_list_t[x-1],n_list_dp[x-1])
+    d_list.append(n_list_p[x-1]-n_list_dp[x-1])
 
-gl = n_list_p[-1]-n_list_p[0]
-lm = gl/len(n_list_t)
-turtle.penup()
-turtle.goto(n_list_t[0],n_list_p[0])
-turtle.pendown()
+bd = 0
+sd = 0
 
-for x in range (120):
-    turtle.goto(n_list_t[x],n_list_t[x]*lm)
+for x in range (50):
+    if bd < d_list[x]:
+        bd = d_list[x]
+    if sd > d_list[x]:
+        sd = d_list[x]
+for x in range (50):
+    ulc.append(n_list_dp[x] + bd)
+    llc.append(n_list_dp[x] + sd)
+
+ft = 1
+for x in range (50):
+    if ft == 1:
+        turtle.penup()
+    else:
+        turtle.pendown()
+    x += 1
+    ft = 0
+    turtle.goto(n_list_t[x-1],ulc[x-1])
+ft = 1
+for x in range (50):
+    if ft == 1:
+        turtle.penup()
+    else:
+        turtle.pendown()
+    x += 1
+    ft = 0
+    turtle.goto(n_list_t[x-1],llc[x-1])
+
 
 input()
